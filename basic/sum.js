36e19386e3abe8518adfc5535711a4874ffd5314
Infiltrators-Elixir <bhst1124@gmail.com>
2021-12-26 14:51:53 +0800

    sum V2.0

--- basic/sum.js
+++ basic/sum.js
@@ -1,14 +1,21 @@
+//從0開始加到n
 function sum(n) {
-    
+
     let result = 0;
-    for (i= 1 ; i <= n ; i++ ) {
-        result= i + result;
-    }
-      
-    return result;
+        for (i = 0; i <= Math.abs(n); i++) {
+            result = i + result;
+        }
+
+    if(n>0){
+    return result}
+    else if(n=0){return 0}
+    else {return -result}
 };
 
 console.log(sum(1));
 console.log(sum(2));
 console.log(sum(5));
 console.log(sum(10));
+console.log(sum(-50));
+
+console.log(sum(-20)+sum(40));
